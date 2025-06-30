5455fb441bdd2c93f403a9c8c731e15fb65cf96c
Jonas Greifenhain <cadivus@daverkomp.de>
2025-06-30 16:29:10 -0700

    `offload/languages`: Reduce initial feature set

--- offload/languages/hip/src/hip_runtime.cpp
+++ offload/languages/hip/src/hip_runtime.cpp
@@ -17,15 +17,5 @@
 
 #include "../../kernel/src/LanguageRegistration.cpp"
 
-#include "../../kernel/src/LanguageLaunch.cpp"
-
 // Must be last as it introduces alises for some definitions from above.
 #include "LanguageAliases.h"
-
-extern "C" hipError_t hipLaunchKernel(const char *KernelID, dim3 GridDim,
-                                      dim3 BlockDim, void **KernelArgsPtr,
-                                      size_t DynamicSharedMem, void *Stream) {
-  return olKConvertResult(llvmLaunchKernelImpl(KernelID, GridDim, BlockDim,
-                                               KernelArgsPtr, DynamicSharedMem,
-                                               Stream, nullptr));
-}
